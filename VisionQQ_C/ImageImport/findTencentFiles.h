521ca75456f276a39d2c72d4d46ce2ff508d6862
Na2Cr2O7 <SchoolF21@outlook.com>
2026-06-19 11:39:14 +0800

    remove unused

--- VisionQQ_C/ImageImport/findTencentFiles.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "pch.h"
-//#include "findTencentFiles.cpp"
-std::optional<std::wstring> findTencentFiles();
-void findEveryFiles2(const std::filesystem::path& dir, std::vector<std::filesystem::path>& files);
-void listFolders(const std::filesystem::path& path, std::vector<std::filesystem::path>& files);
-bool folderValid(const std::filesystem::path& path);
